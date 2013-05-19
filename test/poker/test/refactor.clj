3651e4b56c1f7897c836caf491fc3d59012e005b
Chris Ford <christophertford@gmail.com>
2013-05-19 20:15:34 +0300

    Test unthreading naked fns.

--- test/poker/test/refactor.clj
+++ test/poker/test/refactor.clj
@@ -10,6 +10,10 @@
   (refactor/unthread '(clojure.core/->> [2 3] (map inc) (filter even?))) =>
   '(filter even? (map inc [2 3])))
 
+(fact "Unthreading ->> understands naked fns." 
+  (refactor/unthread '(clojure.core/->> [2 3] first)) =>
+  '(first [2 3]))
+
 (fact "Threading-last replaces nested forms with a ->> pipeline." 
   (refactor/thread-last '(filter even? (map inc [2 3]))) =>
   '(->> [2 3] (map inc) (filter even?)))
@@ -21,6 +25,10 @@
 (fact "Unthreading works with a fully-qualified ->." 
   (refactor/unthread '(clojure.core/-> {:foo 2} (assoc :bar 1) (update-in [:foo] inc))) =>
   '(update-in (assoc {:foo 2} :bar 1) [:foo] inc))
+
+(fact "Unthreading -> understands naked fns." 
+  (refactor/unthread '(clojure.core/-> 1 inc)) =>
+  '(inc 1))
 
 (fact "Threading-first replaces nested forms with a -> pipeline." 
   (refactor/thread-first '(update-in (assoc {:foo 2} :bar 1) [:foo] inc)) =>
